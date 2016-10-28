5c4a24b8e9267163d94a5f64e8fcb186ec88ccc7
Walther <veeti.haapsamo@gmail.com>
2016-10-28 12:21:14 +0300

    Herokufying

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -21,6 +21,11 @@ import tikape.runko.domain.Viesti;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+		// asetetaan portti jos heroku antaa PORT-ympäristömuuttujan
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
+
         Database database = new Database("jdbc:sqlite:keskustelu.db");
         database.init();
 

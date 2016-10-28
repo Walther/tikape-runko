ca0f8086f8c263300273b29bfefe93ed0f51f8d3
Walther <veeti.haapsamo@gmail.com>
2016-10-28 12:41:18 +0300

    Add postgresql support

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -26,7 +26,14 @@ public class Main {
             port(Integer.valueOf(System.getenv("PORT")));
         }
 
-        Database database = new Database("jdbc:sqlite:keskustelu.db");
+		// käytetään oletuksena paikallista sqlite-tietokantaa
+        String jdbcOsoite = "jdbc:sqlite:keskustelu.db";
+        // jos heroku antaa käyttöömme tietokantaosoitteen, otetaan se käyttöön
+        if (System.getenv("DATABASE_URL") != null) {
+            jdbcOsoite = System.getenv("DATABASE_URL");
+        } 
+
+        Database database = new Database(jdbcOsoite);
         database.init();
 
         KayttajaDao kayttajaDao = new KayttajaDao(database);
--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -12,12 +12,13 @@ public class Database {
         this.databaseAddress = databaseAddress;
     }
 
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(databaseAddress);
-    }
-
-    public void init() {
-        List<String> lauseet = sqliteLauseet();
+   private void init() {
+        List<String> lauseet = null;
+        if (this.databaseAddress.contains("postgres")) {
+            lauseet = postgreLauseet();
+        } else {
+            lauseet = sqliteLauseet();
+        }
 
         // "try with resources" sulkee resurssin automaattisesti lopuksi
         try (Connection conn = getConnection()) {
@@ -34,7 +35,26 @@ public class Database {
             System.out.println("Error >> " + t.getMessage());
         }
     }
-    
+
+	public Connection getConnection() throws SQLException {
+        if (this.databaseAddress.contains("postgres")) {
+            try {
+                URI dbUri = new URI(databaseAddress);
+
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+                return DriverManager.getConnection(dbUrl, username, password);
+            } catch (Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+                t.printStackTrace();
+            }
+        }
+
+        return DriverManager.getConnection(databaseAddress);
+    }
+
     public int update(String updateQuery, Object... params) throws SQLException {
         PreparedStatement stmt = getConnection().prepareStatement(updateQuery);
 
@@ -62,7 +82,22 @@ public class Database {
         lista.add("INSERT INTO ALUE (alueID, nimi) VALUES (2, 'Elämä');");
         lista.add("INSERT INTO LANKA (lankaID, nimi, alueID) VALUES (1, 'Tikape', 1);");
         lista.add("INSERT INTO VIESTI (viestiID, kayttajaID, lankaid, aika, viesti) VALUES (1, 1, 1, '2016-10-21T00:50:16Z', 'Osaamme ohjelmoida!');");
-        
+
         return lista;
     }
+
+	private List<String> postgreLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("CREATE TABLE ALUE (alueID SERIAL PRIMARY KEY, nimi varchar(50));");
+        lista.add("CREATE TABLE LANKA (lankaID SERIAL PRIMARY KEY, alueID integer NOT NULL, nimi varchar(50), FOREIGN KEY(alueID) REFERENCES ALUE(alueID));");
+        lista.add("CREATE TABLE KAYTTAJA (kayttajaID SERIAL PRIMARY KEY, nimi varchar(50));");
+        lista.add("CREATE TABLE VIESTI (viestiID SERIAL PRIMARY KEY, kayttajaID integer NOT NULL, lankaID integer NOT NULL, aika date, viesti varchar(300), FOREIGN KEY(kayttajaID) REFERENCES KAYTTAJA(kayttajaID), FOREIGN KEY(lankaID) REFERENCES LANKA(lankaID));");
+        lista.add("INSERT INTO KAYTTAJA (kayttajaID, nimi) VALUES (1, 'Joonas');");
+        lista.add("INSERT INTO KAYTTAJA (kayttajaID, nimi) VALUES (2, 'Mari');");
+        lista.add("INSERT INTO ALUE (alueID, nimi) VALUES (1, 'Opiskelu');");
+        lista.add("INSERT INTO ALUE (alueID, nimi) VALUES (2, 'Elämä');");
+        lista.add("INSERT INTO LANKA (lankaID, nimi, alueID) VALUES (1, 'Tikape', 1);");
+        lista.add("INSERT INTO VIESTI (viestiID, kayttajaID, lankaid, aika, viesti) VALUES (1, 1, 1, '2016-10-21T00:50:16Z', 'Osaamme ohjelmoida!');");
+    }
+
 }
